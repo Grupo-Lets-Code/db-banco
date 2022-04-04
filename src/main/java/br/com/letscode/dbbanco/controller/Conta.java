bed3e297d04976ccb2bec3576dec0289c4fba906
Marcos <marcos.nofre@hotmail.com>
2022-04-04 19:19:30 -0300

    teste

--- src/main/java/br/com/letscode/dbbanco/controller/Conta.java
+++ src/main/java/br/com/letscode/dbbanco/controller/Conta.java
@@ -1,7 +1,36 @@
 package br.com.letscode.dbbanco.controller;
 
+import br.com.letscode.dbbanco.entities.conta.TipoConta;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@ToString
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
 public class Conta {
 
-    // Receber os dados da View e instanciar o ContaFactory
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int conta_id;
+    public int cliente_id;
+    public int agencia_id;
+    public int numero;
+    public char senha;
+    public Date data_abertura;
+
+    @Enumerated(EnumType.STRING)
+    private TipoConta tipoconta;
+
+    public BigDecimal saldo;
+
 
 }
--- src/main/java/br/com/letscode/dbbanco/entities/conta/ContaDefault.java
+++ src/main/java/br/com/letscode/dbbanco/entities/conta/ContaDefault.java
@@ -1,12 +1,17 @@
 package br.com.letscode.dbbanco.entities.conta;
 
 import br.com.letscode.dbbanco.entities.cliente.Cliente;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Setter
+@Getter
 public class ContaDefault {
 
     @Id
@@ -14,7 +19,10 @@ public class ContaDefault {
     private Cliente cliente;
 
     private BigDecimal saldo = BigDecimal.valueOf(0);
+
+    @Enumerated(EnumType.STRING)
     private TipoConta tipoConta;
+
     private String agencia;
     private long numeroConta;
     private String senha;
