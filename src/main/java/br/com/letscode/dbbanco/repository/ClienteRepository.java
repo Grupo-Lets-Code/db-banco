d09401ca193ab92cea64cca58b6221266ca50f98
RafaelLopreti <rafael.lopreti@gmail.com>
2022-04-05 20:46:38 -0300

    First Cliente Commit

--- src/main/java/br/com/letscode/dbbanco/repository/ClienteRepository.java
+++ src/main/java/br/com/letscode/dbbanco/repository/ClienteRepository.java
@@ -1,4 +1,12 @@
 package br.com.letscode.dbbanco.repository;
 
-public interface ClienteRepository {
+import br.com.letscode.dbbanco.entities.cliente.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Query(value = "SELECT MAX(ID) FROM CLIENTE", nativeQuery = true)
+    int catTheLastId();
+
 }
