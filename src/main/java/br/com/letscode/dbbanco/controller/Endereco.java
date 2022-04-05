9e29199b6e7a9dfc1b80b0b2406de012aa06f32b
Rafael Alves Dorta <rafael.alves.dorta@gmail.com>
2022-04-05 16:40:15 -0300

    att2

--- src/main/java/br/com/letscode/dbbanco/controller/Endereco.java
+++ src/main/java/br/com/letscode/dbbanco/controller/Endereco.java
@@ -1,8 +1,32 @@
 package br.com.letscode.dbbanco.controller;
+import java.util.Scanner;
 
 public class Endereco {
 
+
+
+
+
     public static void MapearEndereco(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o logradouro:");
+            String lagradouro = input.next();
+        System.out.println("Digite o numero:");
+            String numero = input.next();
+        System.out.println("Digite a cidade:");
+            String cidade = input.nextLine();
+        System.out.println("Digite o bairro:");
+            String bairro = input.nextLine();
+        System.out.println("Digite o estado:");
+            String estado = input.nextLine();
+        System.out.println("Digite o pais:");
+            String pais = input.nextLine();
+        System.out.println("Digite o cep (Só os números):");
+            int cep = input.nextInt();
+
+
+
 
     }
 
