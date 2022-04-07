fd8c4a23803a566502357da63fabbe951c6ab04a
Marcos <marcos.nofre@hotmail.com>
2022-04-07 20:39:50 -0300

    menu main

--- src/main/java/br/com/letscode/dbbanco/view/Menu.java
+++ src/main/java/br/com/letscode/dbbanco/view/Menu.java
@@ -1,6 +1,7 @@
 package br.com.letscode.dbbanco.view;
 
 import br.com.letscode.dbbanco.controller.ClienteController;
+import br.com.letscode.dbbanco.controller.ContaController;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -12,9 +13,11 @@ public class Menu {
     static Scanner input = new Scanner(System.in);
 
     private final ClienteController clienteController;
+        private final ContaController contaController;
 
-    public Menu(ClienteController clienteController) {
+    public Menu(ClienteController clienteController, ContaController contaController) {
         this.clienteController = clienteController;
+            this.contaController = contaController;
     }
 
     public void painelInicio() {
@@ -187,7 +190,11 @@ public class Menu {
         Scanner input = new Scanner(System.in);
 
         System.out.println("\nDigite o número da conta: ");
-        String numeroConta = input.nextLine();
+        Integer numeroConta = Integer.valueOf(input.next());
+
+        System.out.println("\nDigite a senha da conta: ");
+        int senha = input.nextInt();
+
 
         //FIXME Validar se a conta existe antes de passar para a senha
         //Exemplo: contaExists(); para verificar se a conta existe
@@ -196,13 +203,16 @@ public class Menu {
         System.out.println("\nConta inválida!");
         painelSacar();
 
-        //FIXME SE CONTA EXISTE:
-        System.out.println("\nDigite a senha da conta: ");
-        String senha = input.nextLine();
+
+
 
         //FIXME SE SENHA CORRETA, então
         System.out.println("\nDigite o valor do saque: ");
         BigDecimal valor = input.nextBigDecimal();
+
+        contaController.sacar(numeroConta, senha, valor);
+        painelSacar();
+
 
         //FIXME SE SENHA INCORRETA, então
         System.out.println("\nSenha inválida!");
@@ -219,7 +229,7 @@ public class Menu {
         Scanner input = new Scanner(System.in);
 
         System.out.println("\nDigite o número da conta que deseja realizar o depósito: ");
-        String numeroConta = input.nextLine();
+        Integer numeroConta = Integer.valueOf(input.next());
 
         //FIXME Validar se a conta existe
         //Exemplo: contaExists(); para verificar se a conta existe
@@ -228,9 +238,12 @@ public class Menu {
         System.out.println("\nConta inválida!");
         painelDepositar();
 
-        //FIXME SE CONTA EXISTE:
+
         System.out.println("\nDigite o valor do depósito: ");
         BigDecimal valor = input.nextBigDecimal();
+
+        contaController.depositar(numeroConta, valor);
+
         System.out.println("\nDepósito no valor de " + valor + "R$ realizado com sucesso! Saldo atual: " /*saldo*/);
     }
 
@@ -238,7 +251,7 @@ public class Menu {
         Scanner input = new Scanner(System.in);
 
         System.out.println("\nDigite o número da conta: ");
-        String numeroConta = input.nextLine();
+        Integer numeroConta = Integer.valueOf(input.next());
 
         //FIXME Validar se a conta existe antes de passar para a senha
         //Exemplo: contaExists(); para verificar se a conta existe
@@ -247,59 +260,65 @@ public class Menu {
         System.out.println("\nConta inválida!");
         painelInvestir();
 
-        //FIXME SE CONTA EXISTE:
-        System.out.println("\nDigite a senha da conta: ");
-        String senha = input.nextLine();
 
-        //FIXME SE SENHA CORRETA, então
         System.out.println("\nDigite o valor do investimento: ");
         BigDecimal valor = input.nextBigDecimal();
+
+        contaController.investir(numeroConta, valor);
+
         System.out.println("\nInvestimento no valor de " + valor + "R$ realizado com sucesso! Saldo atual: " /*saldo*/);
 
-        //FIXME SE SENHA INCORRETA, então
-        System.out.println("\nSenha inválida!");
-        painelInvestir();
     }
 
     protected void painelTransferir() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("\nDigite o número da sua conta: ");
-        String contaRemetente = input.nextLine();
+        Integer contaRemetente = Integer.valueOf(input.next());
 
         System.out.println("\nDigite o número da conta que deseja enviar a transferência: ");
-        String contaDestinataria = input.nextLine();
+        Integer contaDestinataria = Integer.valueOf(input.next());
 
         //FIXME Validar se as contas existem antes de passar para a senha
         //Exemplo: contaExists(); para verificar se as contas existem
 
-        //FIXME SE CONTA REMETENTE NÃO EXISTE:
+        /*
         System.out.println("\nConta remetente inválida!");
-        painelTransferir();
+        contaRemetente = Integer.valueOf(input.next());
 
-        //FIXME SE CONTA DESTINATARIA NÃO EXISTE:
+
         System.out.println("\nConta destinatária inválida!");
-        painelTransferir();
+        contaDestinataria = Integer.valueOf(input.next());
+        */
 
-        //FIXME SE AS CONTAS EXISTEM:
+
         System.out.println("\nDigite a senha da sua conta: ");
-        String senha = input.nextLine();
+        int senha = input.nextInt();
+
+        System.out.println("\nSenha inválida!");
+        painelTransferir();
 
         //FIXME SE SENHA CORRETA, então
         System.out.println("\nDigite o valor da transferência: ");
         BigDecimal valor = input.nextBigDecimal();
+
+        contaController.transferir(contaRemetente,contaDestinataria,senha,valor);
+
         System.out.println("\nTransferência no valor de " + valor + "R$ realizada com sucesso! Saldo atual: " /*saldo*/);
 
-        //FIXME SE SENHA INCORRETA, então
-        System.out.println("\nSenha inválida!");
-        painelTransferir();
+
     }
 
     protected void painelSaldo() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("\nDigite o número da conta: ");
-        String numeroConta = input.nextLine();
+        Integer numeroConta = Integer.valueOf(input.next());
+
+        System.out.println("\nDigite a senha da conta: ");
+        int senha = input.nextInt();
+
+        contaController.consultarSaldo(numeroConta, senha);
 
         //FIXME Validar se a conta existe antes de passar para a senha
         //Exemplo: contaExists(); para verificar se a conta existe
@@ -308,15 +327,198 @@ public class Menu {
         System.out.println("\nConta inválida!");
         painelSaldo();
 
-        //FIXME SE CONTA EXISTE:
-        System.out.println("\nDigite a senha da conta: ");
-        String senha = input.nextLine();
-
-        //FIXME SE SENHA CORRETA, então
-        //MOSTRAR OS DADOS DA CONTA
 
         //FIXME SE SENHA INCORRETA, então
         System.out.println("\nSenha inválida!");
         painelSaldo();
     }
+}
+
+package br.com.letscode.dbbanco.view;
+
+        import br.com.letscode.dbbanco.controller.ClienteController;
+        import br.com.letscode.dbbanco.controller.ContaController;
+        import org.springframework.stereotype.Component;
+
+        import java.math.BigDecimal;
+        import java.util.Scanner;
+
+@Component
+public class Menu {
+
+    static Scanner input = new Scanner(System.in);
+
+    private final ClienteController clienteController;
+
+
+    }
+
+    public void painelInicio() {
+        System.out.println(" ----------------------------------------------------- ");
+        System.out.println("|⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ Bem-vindo ao⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀|");
+        System.out.println("|⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀$⠀Banco Grupo Azul⠀$⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀|");
+        System.out.println("|⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ |");
+        System.out.println("|⠀⠀⠀⠀⠀⠀Selecione a operação que deseja realizar⠀⠀⠀⠀⠀⠀⠀ |");
+        System.out.println(" ----------------------------------------------------- ");
+        System.out.println("|    Opção 1 - Abrir conta         |");
+        System.out.println("|    Opção 2 - Sacar               |");
+        System.out.println("|    Opção 3 - Depositar           |");
+        System.out.println("|    Opção 4 - Transferir          |");
+        System.out.println("|    Opção 5 - Investir            |");
+        System.out.println("|    Opção 6 - Consultar saldo     |");
+        System.out.println("|    Opção 7 - Sair                |");
+
+        int operacao = input.nextInt();
+
+        switch (operacao) {
+            case 1:
+                painelPessoa();
+                break;
+            case 2:
+                painelSacar();
+                break;
+            case 3:
+                painelDepositar();
+                break;
+            case 4:
+                painelTransferir();
+                break;
+            case 5:
+                painelInvestir();
+                break;
+            case 6:
+                painelSaldo();
+                break;
+            case 7:
+                System.out.println("Obrigado por utilizar o Banco Grupo Azul!");
+                System.exit(0);
+            default:
+                System.out.println("Opção inválida!");
+                painelInicio();
+                break;
+        }
+    }
+
+    protected void painelPessoa() {
+        System.out.println("|    Opção 1 - Pessoa Física      |");
+        System.out.println("|    Opção 2 - Pessoa Jurídica    |");
+        System.out.println("|    Opção 3 - Voltar             |");
+
+        int tipoPessoa = input.nextInt();
+
+        switch (tipoPessoa) {
+            case 1:
+                tipoPF();
+                break;
+            case 2:
+                tipoPJ();
+                break;
+            case 3:
+                painelInicio();
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                painelPessoa();
+                break;
+        }
+    }
+
+    protected void tipoPF() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nDigite seu nome: ");
+        String nome = input.nextLine();
+
+        System.out.println("\nDigite seu email: ");
+        String email = input.nextLine();
+
+        System.out.println("\nDigite seu telefone: ");
+        String telefone = input.nextLine();
+
+        System.out.println("\nDigite seu CPF: ");
+        String cpf = input.nextLine();
+
+        System.out.println("\nDigite sua data de nascimento: ");
+        String data_nascimento = input.nextLine();
+
+        clienteController.createPF(nome, email, telefone, cpf, data_nascimento);
+        painelContasPF();
+    }
+
+    protected void tipoPJ() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nDigite sua razão social: ");
+        String nome = input.nextLine();
+
+        System.out.println("\nDigite seu email corporativo: ");
+        String email = input.nextLine();
+
+        System.out.println("\nDigite seu telefone: ");
+        String telefone = input.nextLine();
+
+        System.out.println("\nDigite seu CNPJ: ");
+        String cnpj = input.nextLine();
+
+        System.out.println("\nDigite a data de abertura da empresa: ");
+        String data_abertura = input.nextLine();
+
+        clienteController.createPJ(nome, email, telefone, cnpj, data_abertura);
+        painelContasPJ();
+    }
+
+    protected void painelContasPF() {
+        System.out.println("|    Opção 1 - Conta Corrente           |");
+        System.out.println("|    Opção 2 - Conta Poupança           |");
+        System.out.println("|    Opção 3 - Conta Investimento       |");
+        System.out.println("|    Opção 4 - Voltar                   |");
+
+        int tipoConta = input.nextInt();
+
+        switch (tipoConta) {
+            case 1:
+                //Chamar o metodo abrir conta corrente
+                break;
+            case 2:
+                //Chamar o metodo abrir conta poupança
+                break;
+            case 3:
+                //Chamar o metodo abrir conta investimento
+                break;
+            case 4:
+                painelPessoa();
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                painelContasPF();
+                break;
+        }
+    }
+
+    protected void painelContasPJ() {
+        System.out.println("|    Opção 1 - Conta Corrente           |");
+        System.out.println("|    Opção 2 - Conta Investimento       |");
+        System.out.println("|    Opção 3 - Voltar                   |");
+
+        int tipoContaPJ = input.nextInt();
+
+        switch (tipoContaPJ) {
+            case 1:
+                //Banco.criarCorrentePJ();
+                break;
+            case 2:
+                //Banco.criarInvestimentoPJ();
+                break;
+            case 3:
+                painelPessoa();
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                tipoPJ();
+                break;
+        }
+    }
+
+
+
 }
