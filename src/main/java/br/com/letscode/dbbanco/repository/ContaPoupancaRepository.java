61825b7947bcbd3cd783f7b73f4778d2eacd74c1
CaioPaiva1911 <caiowick@hotmail.com>
2022-04-08 10:54:02 -0300

    exclude repositories nao utilizadas

--- src/main/java/br/com/letscode/dbbanco/repository/ContaPoupancaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.letscode.dbbanco.repository;
-
-import br.com.letscode.dbbanco.entities.conta.Conta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContaPoupancaRepository extends JpaRepository<Conta, Integer> {
-}
