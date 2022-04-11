88de7b78d07d5f0775ebef23ee8306fd8def11ab
RafaelLopreti <rafael.lopreti@gmail.com>
2022-04-11 00:23:18 -0300

    Remove Query in ClienteRepository

--- src/main/java/br/com/letscode/dbbanco/repository/ClienteRepository.java
+++ src/main/java/br/com/letscode/dbbanco/repository/ClienteRepository.java
@@ -2,11 +2,9 @@ package br.com.letscode.dbbanco.repository;
 
 import br.com.letscode.dbbanco.entities.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
-    @Query(value = "SELECT MAX(ID) FROM CLIENTE", nativeQuery = true)
-    int catTheLastId();
 
 }
