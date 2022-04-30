c2bac74b0fa96afe25cf780e53f4e31a964e0849
Carlos <carlosmsmito@gmail.com>
2022-04-30 00:13:52 -0300

    Created Beans Validation Constraints for ClientePJ

--- src/main/java/br/com/letscode/dbbanco/entities/cliente/ClientePJ.java
+++ src/main/java/br/com/letscode/dbbanco/entities/cliente/ClientePJ.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @ToString
@@ -14,20 +16,23 @@ import java.time.LocalDate;
 @Setter
 @Entity
 @Table(name = "PESSOA_JURIDICA")
-public class ClientePJ{
+public class ClientePJ {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected int id;
 
     @Column(name = "CNPJ", nullable = false)
+    @Pattern(regexp = "^\\d{14}$", message = "Formato de CNPJ inválido - registrar apenas números")
     protected String CNPJ;
 
     @Column(name = "Data_Abertura", nullable = false)
+    @NotNull(message = "O cliente deve informar a data de abertura da empresa")
     protected LocalDate dataDeAbertura;
 
     @OneToOne
     @JoinColumn(name = "Cliente_ID", nullable = false)
+    @NotNull(message = "O cliente pessoa jurídica deve estar associado à uma entidade cliente")
     protected Cliente cliente;
 
     public ClientePJ(String CNPJ, LocalDate dataDeAbertura, Cliente cliente) {
