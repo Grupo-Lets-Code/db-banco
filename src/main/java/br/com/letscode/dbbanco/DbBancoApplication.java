976f6dbe1be61532cf913bfdb80d675d0dd6650f
Carlos <carlosmsmito@gmail.com>
2022-04-29 22:58:45 -0300

    Changed main into a Command Line Runner application

--- src/main/java/br/com/letscode/dbbanco/DbBancoApplication.java
+++ src/main/java/br/com/letscode/dbbanco/DbBancoApplication.java
@@ -2,6 +2,7 @@ package br.com.letscode.dbbanco;
 
 import br.com.letscode.dbbanco.controller.ContaController;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +10,19 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 @Slf4j
 @Configuration
-public class DbBancoApplication{
+public class DbBancoApplication implements CommandLineRunner {
     public static void main(String[] args) {
         try{
-            SpringApplication.run(DbBancoApplication.class, args);
+            // NOTE: Retire o .close() para manter a aplicação rodando
+            SpringApplication.run(DbBancoApplication.class, args).close();
         } catch (Exception e){
             System.out.println("Erro");
         }
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        // Teste da validação pelo Bean Annotation
+        System.out.println("Carlos");
     }
 }
