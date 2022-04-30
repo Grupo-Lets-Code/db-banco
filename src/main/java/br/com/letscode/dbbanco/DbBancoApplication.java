4f9e5fc1dd8dece8015adabc42c9a6ff822a7324
unknown <marcosfranciosi@live.com>
2022-04-30 19:21:55 -0300

    Testes Iniciais

--- src/main/java/br/com/letscode/dbbanco/DbBancoApplication.java
+++ src/main/java/br/com/letscode/dbbanco/DbBancoApplication.java
@@ -1,6 +1,8 @@
 package br.com.letscode.dbbanco;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +10,25 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 @Slf4j
 @Configuration
+<<<<<<< HEAD
 public class DbBancoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DbBancoApplication.class, args);
+=======
+public class DbBancoApplication{
+    private static final Logger LOGGER = LoggerFactory.getLogger(DbBancoApplication.class);
+    public static void main(String[] args) {
+        try{
+            LOGGER.info("\n ===>>> Iniciando Aplicação Banco Grupo Azul <<<===");
+//            SpringApplication.run(DbBancoApplication.class, args);
+            LOGGER.info("\n=============================================================== \n" +
+                    "                 Banco Grupo Azul\n" +
+                    "===============================================================");
+            throw new Exception();
+        } catch (Exception e){
+            LOGGER.error("Erro ao iniciar a Aplicação", e);
+        }
+
+>>>>>>> 3cae86f (Testes Iniciais)
     }
 }
