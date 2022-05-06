b63f28aa0ba9d73f5cc7aee4246c4cc246a45e76
Natália Satie <naty.odashima@gmail.com>
2022-05-06 14:14:34 -0300

    adicionei dependencias e um arquivo rascunho

--- src/test/java/br/com/letscode/dbbanco/DbBancoApplicationTests.java
+++ src/test/java/br/com/letscode/dbbanco/DbBancoApplicationTests.java
@@ -1,13 +1,28 @@
 package br.com.letscode.dbbanco;
 
+import br.com.letscode.dbbanco.controller.ClienteController;
+import br.com.letscode.dbbanco.entities.cliente.Cliente;
+import br.com.letscode.dbbanco.repository.ClienteRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DbBancoApplication.class)
 class DbBancoApplicationTests {
 
+    @MockBean
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ClienteController clienteController;
+
     @Test
-    void contextLoads() {
+    void loadCliente() {
+        Cliente cliente = clienteRepository.getById(1);
+        ResultSearch movieOneResult = getMovieResult(61);
     }
 
 }
