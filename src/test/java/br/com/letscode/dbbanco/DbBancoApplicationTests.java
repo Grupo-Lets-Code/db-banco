e71a597b40f21db13949b247d36a806eeeba9ae3
Natália Satie <naty.odashima@gmail.com>
2022-05-09 10:47:18 -0300

    criar teste

--- src/test/java/br/com/letscode/dbbanco/DbBancoApplicationTests.java
+++ src/test/java/br/com/letscode/dbbanco/DbBancoApplicationTests.java
@@ -3,15 +3,18 @@ package br.com.letscode.dbbanco;
 import br.com.letscode.dbbanco.controller.ClienteController;
 import br.com.letscode.dbbanco.entities.cliente.Cliente;
 import br.com.letscode.dbbanco.repository.ClienteRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = DbBancoApplication.class)
+@ExtendWith(MockitoExtension.class)
 class DbBancoApplicationTests {
 
     @MockBean
@@ -20,9 +23,16 @@ class DbBancoApplicationTests {
     private ClienteController clienteController;
 
     @Test
-    void loadCliente() {
-        Cliente cliente = clienteRepository.getById(1);
-        ResultSearch movieOneResult = getMovieResult(61);
+    void salvarClienteTeste() {
+        Cliente clienteSalvar = new Cliente("Teste", "test@gmail.com", "(11) 90099-0040");
+
+        Cliente clienteRetorno = new Cliente(7, "Teste", "test@gmail.com", "(11) 90099-0040");
+
+        Mockito.when(clienteRepository.save(clienteSalvar))
+                .thenReturn(clienteRetorno);
+
+        Assertions.assertEquals(clienteSalvar.getNome(), clienteRetorno.getNome());
+        Assertions.assertEquals(clienteSalvar.getId(), clienteRetorno.getId());
     }
 
 }
