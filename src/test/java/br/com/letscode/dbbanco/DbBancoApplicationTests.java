c5db360b6eeb1b1a2631e1cf5d00f42f9d003f9c
Natália Satie <naty.odashima@gmail.com>
2022-05-09 19:01:24 -0300

    adição de asserts

--- src/test/java/br/com/letscode/dbbanco/DbBancoApplicationTests.java
+++ src/test/java/br/com/letscode/dbbanco/DbBancoApplicationTests.java
@@ -3,6 +3,7 @@ package br.com.letscode.dbbanco;
 import br.com.letscode.dbbanco.controller.ClienteController;
 import br.com.letscode.dbbanco.entities.cliente.Cliente;
 import br.com.letscode.dbbanco.repository.ClienteRepository;
+import br.com.letscode.dbbanco.service.ClienteService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,25 +15,29 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class DbBancoApplicationTests {
 
     @MockBean
     private ClienteRepository clienteRepository;
     @Autowired
-    private ClienteController clienteController;
+    private ClienteService clienteService;
 
     @Test
     void salvarClienteTeste() {
         Cliente clienteSalvar = new Cliente("Teste", "test@gmail.com", "(11) 90099-0040");
-
         Cliente clienteRetorno = new Cliente(7, "Teste", "test@gmail.com", "(11) 90099-0040");
 
         Mockito.when(clienteRepository.save(clienteSalvar))
-                .thenReturn(clienteRetorno);
+                        .thenReturn(clienteRetorno);
 
-        Assertions.assertEquals(clienteSalvar.getNome(), clienteRetorno.getNome());
-        Assertions.assertEquals(clienteSalvar.getId(), clienteRetorno.getId());
+        clienteRetorno = clienteService.salvarCliente(clienteSalvar);
+
+        Assertions.assertEquals(clienteRetorno.getNome(), clienteSalvar.getNome());
+        Assertions.assertEquals(clienteRetorno.getId(), 7);
     }
 
 }
