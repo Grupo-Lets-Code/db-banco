0ebd996f8be19623a4de56c70209494f9a5d008a
Natália Satie <naty.odashima@gmail.com>
2022-05-13 09:28:27 -0300

    add exists

--- src/main/java/br/com/letscode/dbbanco/repository/ClientePFRepository.java
+++ src/main/java/br/com/letscode/dbbanco/repository/ClientePFRepository.java
@@ -1,13 +1,11 @@
 package br.com.letscode.dbbanco.repository;
 
 import br.com.letscode.dbbanco.entities.cliente.ClientePF;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ClientePFRepository extends JpaRepository<ClientePF, Integer> {
+    @Override
+    boolean existsById(Integer id);
 }
